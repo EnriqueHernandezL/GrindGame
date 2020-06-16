86d5d1ea5a7be22ece4c510685014c06650c73d4
EnriqueHernandezL <enrique.hl.7@hotmail.com>
2020-06-16 21:34:15 +0200

    LiveData refactor, finish day button, daily goal

--- app/src/main/java/com/enriher/grindgame/TimerFragment.kt
+++ app/src/main/java/com/enriher/grindgame/TimerFragment.kt
@@ -14,13 +14,8 @@ import com.enriher.grindgame.databinding.FragmentTimerBinding
 class TimerFragment : Fragment() {
 
     private lateinit var viewModel: TimerViewModel
-
     private lateinit var binding: FragmentTimerBinding
-
-    private var handler = Handler()
-
-    private lateinit var runnable: Runnable
-
+    
     override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,91 +24,28 @@ class TimerFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_timer, container, false)
         viewModel = ViewModelProviders.of(this).get(TimerViewModel::class.java)
 
-        if(viewModel.timerRunning) {
-            startTimer()
-        }
-        if(viewModel.editingTime) {
-            editTime()
-        }
+        // Data binding working with LiveData
+        binding.timerViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
-        binding.time = viewModel.secondsToTime()
-        binding.buttonStart.setOnClickListener { handleStartButton() }
         binding.buttonEdit.setOnClickListener { handleEditButton() }
+
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        if(viewModel.timerRunning) {
-            handler.removeCallbacks(runnable)
-        }
-    }
-
     private fun handleEditButton() {
-        if(viewModel.editingTime) {
-            applyEditTime()
+        if(viewModel.editingTime.value ?: false) {
+            viewModel.applyEditTime(binding.textEditClock.text.toString())
         } else {
             editTime()
         }
-        viewModel.editingTime = !viewModel.editingTime
-    }
-
-    private fun handleStartButton() {
-        if(viewModel.timerRunning) {
-            pauseTimer()
-        } else {
-            startTimer()
-        }
-        viewModel.timerRunning = !viewModel.timerRunning
-    }
-
-    private fun startTimer() {
-        binding.buttonStart.setText(R.string.pause)
-        // Create the runnable action
-        runnable = Runnable {
-            viewModel.seconds++
-            binding.time = viewModel.secondsToTime()
-            handler.postDelayed(runnable, 1000)
-        }
-
-        handler.post(runnable)
-    }
-
-    private fun pauseTimer() {
-        binding.buttonStart.setText(R.string.start)
-        handler.removeCallbacks(runnable)
+        viewModel.handleEditButton() // Change boolean flag
     }
 
     private fun editTime() {
-        // If timer running, pause
-        if(viewModel.timerRunning) {
-            handleStartButton()
-        }
+        viewModel.editTime()
         // Set text in input to current time
-        binding.textEditClock.setText(viewModel.secondsToTime())
-
-        // Change Button texts/UI elements visibility
-        binding.buttonEdit.setText(R.string.apply)
-        binding.textClock.visibility = View.INVISIBLE
-        binding.textEditClock.visibility = View.VISIBLE
-    }
-
-    private fun applyEditTime() {
-        // TODO extract in method and move to viewModel
-        val input = binding.textEditClock.text.toString().split(":")
-        if(input.size != 3) {
-            // TODO: Handle error
-            // TODO: Handle no Int inputs
-            return
-        }
-        viewModel.seconds = input[0].toInt() * 3600 + input[1].toInt() * 60 + input[2].toInt()
-
-        binding.textClock.text = viewModel.secondsToTime()
-
-        // Change Button texts/UI elements visibility
-        binding.buttonEdit.setText(R.string.edit)
-        binding.textClock.visibility = View.VISIBLE
-        binding.textEditClock.visibility = View.GONE
+        binding.textEditClock.setText(viewModel.currentTimeString.value)
     }
 
 }
--- app/src/main/java/com/enriher/grindgame/TimerViewModel.kt
+++ app/src/main/java/com/enriher/grindgame/TimerViewModel.kt
@@ -1,21 +1,123 @@
 package com.enriher.grindgame
 
 import android.os.Handler
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class TimerViewModel : ViewModel() {
-    var seconds = 0
-    var timerRunning = false
-    var editingTime = false
 
-    init {
+    // Fragment data
 
+    // TODO: Set in the app
+    private val dailyGoal = 28800
+
+    private val _seconds = MutableLiveData<Int>()
+    val seconds: LiveData<Int>
+        get() = _seconds
+
+    private val _timerRunning = MutableLiveData<Boolean>()
+    val timerRunning: LiveData<Boolean>
+        get() = _timerRunning
+
+    private val _editingTime = MutableLiveData<Boolean>()
+    val editingTime: LiveData<Boolean>
+        get() = _editingTime
+
+    // Data processed to UI
+    val buttonStartText = Transformations.map(_timerRunning) { running ->
+        if (running) "Pause" else "Start"
+    }
+    val buttonEditText = Transformations.map(_editingTime) { editing ->
+        if (editing) "Apply" else "Edit"
     }
 
-    fun secondsToTime(): String {
-        val hours = seconds / 3600
-        val minutes = (seconds % 3600) / 60
-        val seconds = seconds % 60
-        return "${String.format("%02d", hours)}:${String.format("%02d", minutes)}:${String.format("%02d",seconds)}"
+    val currentTimeString = Transformations.map(seconds) { seconds ->
+        secondsToTime(seconds)
+    }
+
+    val dailyProgess = Transformations.map(seconds) {seconds ->
+        (100 * seconds) / dailyGoal
+    }
+
+    private var handler = Handler()
+    private lateinit var runnable: Runnable
+
+    init {
+        _seconds.value = 0
+        _timerRunning.value = false
+        _editingTime.value = false
+    }
+
+    fun handleEditButton() {
+        _editingTime.value = !(_editingTime.value ?: false)
+    }
+
+    fun editTime() {
+        // If timer running, pause
+        if(timerRunning.value == true) {
+            handleStartButton()
+        }
+    }
+
+    fun applyEditTime(time: String) {
+        // TODO extract in method and move to viewModel
+        val input = time.split(":")
+        if(input.size != 3) {
+            // TODO: Handle error
+            // TODO: Handle no Int inputs
+            return
+        }
+        _seconds.value = input[0].toInt() * 3600 + input[1].toInt() * 60 + input[2].toInt()
+    }
+
+    fun handleStartButton() {
+        if(_timerRunning.value == true) {
+            pauseTimer()
+        } else {
+            startTimer()
+        }
+        _timerRunning.value = !(_timerRunning.value ?: false)
+    }
+
+
+    private fun startTimer() {
+        println("startTimer")
+        // Create the runnable action
+        runnable = Runnable {
+            println("_seconds value" + _seconds.value)
+            println("seconds value" + seconds.value)
+            _seconds.value = _seconds.value?.plus(1)
+            handler.postDelayed(runnable, 1000)
+        }
+
+        // Start Timer
+        handler.post(runnable)
+    }
+
+    private fun pauseTimer() {
+        handler.removeCallbacks(runnable)
+    }
+
+    fun finishDay() {
+        // Stop timer if running
+        if(timerRunning.value == true) {
+            handleStartButton()
+        }
+
+        // TODO: Save seconds in database
+        _seconds.value = 0
+    }
+
+    private fun secondsToTime(secondsArg: Int): String {
+        val hours = secondsArg / 3600
+        val minutes = (secondsArg % 3600) / 60
+        val seconds = secondsArg % 60
+        return "${String.format("%02d", hours)}:${String.format(
+            "%02d",
+            minutes
+        )}:${String.format("%02d", seconds)}"
     }
 }
